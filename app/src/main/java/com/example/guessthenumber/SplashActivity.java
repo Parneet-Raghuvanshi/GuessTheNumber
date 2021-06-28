d8bb5402155764c32c7bf214417f6493d7a598a3
Parneet Raghuvanshi <parneetraghuvanshi@gmail.com>
2021-06-28 09:33:42 +0530

    Updating the Cover Photo

--- app/src/main/java/com/example/guessthenumber/SplashActivity.java
+++ app/src/main/java/com/example/guessthenumber/SplashActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent homeintent = new Intent(SplashActivity.this, Dashboard.class);
-        startActivity(homeintent);
-        finish();
+        requestWindowFeature( Window.FEATURE_NO_TITLE);
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent = new Intent(SplashActivity.this, Dashboard.class);
+                startActivity(homeintent);
+                finish();
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 3000);
+
     }
 }
